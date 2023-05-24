5153c2f19cd1eab0479dfe7c08ca3e1a1a350bc6
nguyet.ho <nguyet.ho23@student.passerellesnumeriques.org>
2023-05-25 00:30:43 +0700

    feat<pages>: create `Login` UI

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,45 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{js,jsx,ts,tsx}",],
   theme: {
-    extend: {},
+    screens: {
+      'xxs': '320px',
+      'xs': '480px',
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    },
+    colors: {
+      "BB": "#4A4A4A",
+      "W":"#F6F6F6",
+      "B1":"#4461F2",
+      "B2":"#E3ECF5",
+      "G":"#61676C",
+      "E":"#dbd1c299",
+      "P":"#F03A76",
+      "P2":"#FF90B5"
+    },
+
+    fontWeight: {
+      "thin": '100',
+      "extralight": '200',
+      "light": '300',
+      "normal": '400',
+      "medium": '500',
+      "semibold": '600',
+      "bold": '700',
+      "extrabold": '800',
+      "black": '900',
+
+    },
+    extend: {
+      fontFamily: {
+        sansA: ['poppins', 'sans-serif'],
+        sansB: ['pangolin', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
