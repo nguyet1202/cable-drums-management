d3b7043ffb80b94e4cf081756f4ce1d076e0441a
nguyet.ho <nguyet.ho23@student.passerellesnumeriques.org>
2023-06-03 18:59:43 +0700

    feat<pages>:create view `Request`

---
+++ src/view/pages/planner_team/Request.tsx
@@ -0,0 +1,108 @@
+import { useState, useEffect } from "react";
+import { child, get, ref } from "firebase/database";
+import { database } from "../../../configs/FirebaseConfig";
+import RequestList from "../../components/data_displays/RequestList";
+import {CreateNewBtn, ModalRequestDetail} from "../../components";
+type RequestData = {
+   contract_id: string;
+   project_contractor_id: string;
+   amount: number;
+   supply_vendor_id: string;
+   status:string;
+   teamname:string;
+   phonenumbers:number;
+   email:string,
+   project_contractor_name:string,
+   project_contractor_phone:string,
+   project_contractor_email:string,
+};
+const Request = () => {
+   const [data, setData] = useState<{ [key: string]: RequestData }>({});
+   const [selectedItem, setSelectedItem] = useState<RequestData | null>(null);
+   const [modalOpen, setModalOpen] = useState<boolean>(false);
+   const [showModal, setShowModal] = useState<boolean>(false);
+
+   useEffect(() => {
+      const dbRef = ref(database);
+      get(child(dbRef, `withdraw_requests`))
+         .then((snapshot) => {
+            if (snapshot.exists()) {
+               setData(snapshot.val());
+            } else {
+               throw new Error("No data available");
+            }
+         })
+         .catch((error) => {
+            throw new Error(error);
+         });
+   }, []);
+
+   const fetchSupplyVendorInfo = async (supplyVendorId: string) => {
+      try {
+         const contractSnapshot = await get(ref(database, `withdraw_requests/${supplyVendorId}`));
+         const contractData = contractSnapshot.val();
+
+         if (contractSnapshot.exists()) {
+            const vendorSnapshot = await get(ref(database, `supply_vendors/${contractData.supply_vendor_id}`));
+            const vendorData = vendorSnapshot.val();
+            if (vendorSnapshot.exists()) {
+               const ProjectorSnapshot = await get(ref(database, `supply_vendors/${contractData.project_contractor_id}`));
+               const ProjectorData = ProjectorSnapshot.val();
+
+               if (vendorSnapshot.exists()) {
+                  setSelectedItem({
+                     contract_id: contractData.contract_id,
+                     project_contractor_id: contractData.project_contractor_id,
+                     project_contractor_name: ProjectorData.teamname,
+                     project_contractor_phone: ProjectorData.phonenumbers,
+                     project_contractor_email: ProjectorData.email,
+                     amount: contractData.amount,
+                     status: contractData.status,
+                     supply_vendor_id: contractData.supply_vendor_id,
+                     teamname: vendorData.teamname,
+                     phonenumbers: vendorData.phonenumbers,
+                     email: vendorData.email
+                  });
+               }
+            }
+         } else {
+            throw new Error("Contract not found");
+         }
+      } catch (error: unknown) {
+         throw new Error(String(error));
+      }
+   };
+
+   const handleOpenModal = (item: RequestData) => {
+      setSelectedItem(item);
+      fetchSupplyVendorInfo(item.supply_vendor_id);
+      setModalOpen(true);
+   };
+
+   const handleCloseModal = () => {
+      setModalOpen(false);
+   };
+   const openModal = () => {
+      setShowModal(true);
+   };
+
+   return (
+      <div className={`${style.wrapper}`}>
+         <CreateNewBtn wrapperStyles={`${style.btnCreate}`} onClick={openModal} />
+         <RequestList data={data}  handleOpenModal={handleOpenModal}  />
+         <ModalRequestDetail open={modalOpen} selectedItem={selectedItem} onClose={handleCloseModal} />
+      </div>
+   );
+};
+
+const style = {
+   wrapper: "w-full flex flex-col bg-W 2xl:px-32 flex items-center justify-center xl:px-16 xs:px-5 lg:px-3 ",
+   button: {
+      size: "xs" as "xs",
+      theme: "A" as "A",
+      wrapperStyles: "py-0 px-0 left-[8%]"
+   },
+   btnCreate: "left-[0%]",
+}
+export default Request;
+export {type RequestData}
