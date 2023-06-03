a3606e743e515e31003ef487722404a4233e7187
nguyet.ho <nguyet.ho23@student.passerellesnumeriques.org>
2023-06-03 18:58:06 +0700

    refactor<Routes>:refactor `Routes`

--- src/routes/PrivateRoute.tsx
+++ src/routes/PrivateRoute.tsx
@@ -1,18 +1,23 @@
-import {Navigate} from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
 import React from 'react';
 
 type PrivateRouteProps = {
    authenticated: boolean
-   role: "admin"|"supply_vendor"|"planner"|"project_contractor"
+   role: "admin" | "supply_vendor" | "planner" | "project_contractor" | ("planner" | "supply_vendor")[]
    element: React.ComponentType;
 }
-export const PrivateRoute = ({element: Element, authenticated, role}: PrivateRouteProps) => {
+
+export const PrivateRoute = ({ element: Element, authenticated, role }: PrivateRouteProps) => {
    const userRole = localStorage.getItem('role');
-   console.log(userRole,authenticated)
-   return (authenticated && userRole === role ? (
-         <Element/>
-      ) : (
-         <Navigate to="login-require" replace/>
-      )
+   console.log(userRole, authenticated);
+
+   const isUserRoleAllowed =
+      typeof userRole === 'string' &&
+      (Array.isArray(role) ? role.includes(userRole as ("planner" | "supply_vendor")) : userRole === role);
+
+   return authenticated && isUserRoleAllowed ? (
+      <Element />
+   ) : (
+      <Navigate to="/login-require" replace />
    );
 };
