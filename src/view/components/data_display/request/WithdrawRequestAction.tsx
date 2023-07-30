34c0e847272b3c11ed26defff8bdced95731b930
nguyet.ho <nguyet.ho23@student.passerellesnumeriques.org>
2023-07-30 23:16:32 +0700

    finish contractor role

--- src/view/components/data_display/request/WithdrawRequestAction.tsx
+++ src/view/components/data_display/request/WithdrawRequestAction.tsx
@@ -7,50 +7,61 @@ import StatusChip from "./StatusChip";
 import * as React from "react";
 
 type WithdrawRequestActionProps = {
-   params: any;
+    params: any;
 };
 
 const WithdrawRequestAction = ({params}: WithdrawRequestActionProps) => {
 
-   const role = useSelector((state: RootState) => state.user.data.role);
+    const role = useSelector((state: RootState) => state.user.data.role);
 
-   const [disabled, setDisabled] = useState<boolean>(true)
-   const [newStatus, setNewStatus] = useState<string>('')
+    const [disabled, setDisabled] = useState<boolean>(true)
+    const [newStatus, setNewStatus] = useState<string>('')
 
-   useEffect(() => {
-      if (role === "supply_vendors" && params.row.status === "new") {
-         setDisabled(false);
-         setNewStatus('ready to collect')
-      } else if (
-         role === "project_contractors" &&
-         params.row.status === "ready to collect"
-      ) {
-         setDisabled(false);
-         setNewStatus('collected')
-      } else {
-         setDisabled(true);
-      }
-   }, [role, params.row.status]);
+    useEffect(() => {
+        if (role === "supply_vendors" && params.row.status === "new") {
+            setDisabled(false);
+            setNewStatus('ready to collect')
+        } else if (
+            role === "project_contractors" &&
+            params.row.status === "ready to collect"
+        ) {
+            setDisabled(false);
+            setNewStatus('collected')
+        } else {
+            setDisabled(true);
+        }
+    }, [role, params.row.status]);
 
-   const updateRequestStatus = async (requestId: string, newstatus: string) => {
-      const requestRef = ref(database, `withdraw_requests/${requestId}`);
-      const snapshot = await get(requestRef);
-      const currentData = snapshot.val();
-      if (currentData.status === ("collected" || "new") && role === "project_contractor") {
-         setDisabled(true)
-      } else {
-         const updatedData = {...currentData, status: newstatus, created_at: new Date().toLocaleString()};
-         await set(requestRef, updatedData);
-         setDisabled(true)
+    const updateContractAmount = async (contractId: string, withdrawAmount: number) => {
+        const contractRef = ref(database, `contracts/${contractId}`);
+        const contractSnapshot = await get(contractRef);
+        const contractData = contractSnapshot.val();
+        const currentAmount = contractData.contract_amount;
+        const updatedAmount = currentAmount - withdrawAmount;
+        await set(contractRef, {...contractData, contract_amount: updatedAmount});
+    };
 
-      }
-   };
+    const updateRequestStatus = async (requestId: string, newstatus: string) => {
+        const requestRef = ref(database, `withdraw_requests/${requestId}`);
+        const snapshot = await get(requestRef);
+        const currentData = snapshot.val();
+        if (currentData.status === ("collected" || "new") && role === "project_contractor") {
+            setDisabled(true)
+        } else {
+            const updatedData = {...currentData, status: newstatus, created_at: new Date().toLocaleString()};
+            await set(requestRef, updatedData);
+            await updateContractAmount(currentData.contract_id, currentData.amount)
+            setDisabled(true)
 
-   return (
-      <div>
-         <StatusChip status={params.row.status} isDisabled={disabled} onClick={() => updateRequestStatus(params.row.id, newStatus)} />
-      </div>
-   );
+        }
+    };
+
+    return (
+        <div>
+            <StatusChip status={params.row.status} isDisabled={disabled}
+                        onClick={() => updateRequestStatus(params.row.id, newStatus)}/>
+        </div>
+    );
 };
 
 export default WithdrawRequestAction;
